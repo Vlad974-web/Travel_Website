745a108333e7aa6835fdc72e88c7a0671509bdbb
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-05-12 00:01:44 +0200

    slider ok

--- ressources/javascript/script.js
+++ ressources/javascript/script.js
@@ -1,8 +1,26 @@
 const menuBtn = document.querySelector('.menu-btn');
 const navigation = document.querySelector('.navigation');
+const btns = document.querySelectorAll('.nav-btn');
 
-
+// Pour le menu
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active');
   navigation.classList.toggle('active');
+});
+
+// Slide video navigation
+  const sliderNav = function (manual) {
+    // Quand tu click sur slide la couleur s'allume
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
+    })
+
+    // Mettre couleur sur slide
+  btns[manual].classList.add('active')
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    sliderNav(i);
+  });
 });
