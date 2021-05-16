c44f5bef3e356c827437df185c114cbb76b4173c
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-05-16 13:50:48 +0200

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Middleware -> BodyParser
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 
 
 // EJS
