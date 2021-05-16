444e20d5c9a4f7ed5cb2b80985c5d18d3e252e28
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-05-16 18:17:03 +0200

    Connecter sur ma BDD grâce au dependance mysql

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
 const app = express();
 
 
@@ -7,13 +8,29 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+
+
 // DotENV
 require('dotenv').config();
 
+// MYSQL -> Connecter à la BDD
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+})
+connection.connect(
+  (err) => {
+    if (err) {
+      throw err
+    }
+    console.log('Connecté au serveur MySQL');
+  }
+)
 
 // EJS
 app.set('view engine', 'ejs');
-
 
 // Dossier STATIC (PUBLIC)
 app.use(express.static(path.join(__dirname, 'public')));
