b68e86014861b19dab7056a193b4b8586081406e
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-05-16 18:03:56 +0200

    déclarer dotenv sur app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+// DotENV
+require('dotenv').config();
+
 
 // EJS
 app.set('view engine', 'ejs');
