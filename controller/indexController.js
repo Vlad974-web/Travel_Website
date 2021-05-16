d7f9823a302c17a2e98878f4cace06498d9afc25
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-05-16 20:13:50 +0200

    Update indexController.js
    Ajouter une requête

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,3 +1,8 @@
 exports.getIndexPage = async (req, res) => {
-  res.render('index');
+
+  // REQUETE MYSQL POUR AFFICHER LES INFOS
+  const listeCategorie = await querysql ('SELECT nom, descriptin, lien FROM categorie');
+
+  // AFFICHER LA PAGE INDEX
+  res.render('index', {categorie: listeCategorie});
 }
